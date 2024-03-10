44605e0fd7e2cb0a70194c4a870052fdf96093e3
eric <ericluismauricio@gmail.com>
2024-03-09 22:04:43 -0300

    add config

---
+++ config/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    development: {
+      username: 'root',
+      password: 'cenapasslogin',
+      database: 'database_cena',
+      host: 'localhost',
+      dialect: 'mysql',
+      logging: console.log
+    },
+    test: {
+      username: 'root',
+      password: 'cenapasslogin',
+      database: 'database_cena',
+      host: 'localhost',
+      dialect: 'mysql',
+      logging: console.log
+    },
+    production: {
+      username: 'root',
+      password: 'cenapasslogin',
+      database: 'database_cena',
+      host: 'localhost',
+      dialect: 'mysql',
+      logging: console.log
+    }
+  };
+  
