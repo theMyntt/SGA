c768bfa413d8ab0f2f4e30e4e41a48af95755371
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-11 21:45:26 -0300

    feat: login route

--- server/main.py
+++ server/main.py
@@ -1,5 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+load_dotenv()
+
+@app.route("api/user/login", methods=["POST"])
+def userLogin():
+  if request.method == "POST":
+    try:
+      post = request.get_json()
+
+      postData = [
+        post.get("EMAIL_USER"),
+        post.get("PASSWORD_USER")
+      ]
+    except Exception as e:
+      return jsonify({"message": "Error: " + str(e)}), 400
