e0597b3a33a7354e3055c5ac1299fdd84e946ea9
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-16 12:09:23 -0300

    fix: sql query

--- server/routes/loginAccount.py
+++ server/routes/loginAccount.py
@@ -1,4 +1,4 @@
 from utils import connectDb
 
 def loginAccount(EMAIL_USER, PASSWORD_USER):
-  return connectDb.connectUsersTable(f"SELECT * FROM USER_INFORMATION WHERE EMAIL_USER = {EMAIL_USER} AND PASSWORD_USER = {PASSWORD_USER}")
+  return connectDb.connectUsersTable(f"SELECT * FROM USER_INFORMATION WHERE EMAIL_USER = '{EMAIL_USER}' AND PASSWORD_USER = '{PASSWORD_USER}'")
