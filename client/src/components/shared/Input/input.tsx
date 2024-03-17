cbb6324ae2f5d3a490379b98cb8c81f0a3b5b3b2
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-17 14:30:24 -0300

    feat: input component

---
+++ client/src/components/shared/Input/input.tsx
@@ -0,0 +1,37 @@
+import "./input.scss";
+
+interface Props {
+  placeholder?: string;
+  type: "string" | "email" | "password";
+  id?: string;
+  maxLength?: number;
+  minLength?: number;
+  className?: string;
+  name?: string;
+}
+
+const Input: React.FC<Props> = (props) => {
+  return (
+    <div className="custom-input-div">
+      <input
+        type={props.type}
+        maxLength={props.maxLength}
+        minLength={props.minLength}
+        className={`custom-input ${props.className}`}
+        placeholder={props.placeholder}
+        id={props.id}
+        name={props.name}
+      />
+    </div>
+  );
+};
+
+Input.defaultProps = {
+  placeholder: "",
+  type: "string",
+  maxLength: undefined,
+  minLength: undefined,
+  className: undefined,
+};
+
+export default Input;
