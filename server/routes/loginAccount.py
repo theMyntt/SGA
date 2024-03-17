03c7c9d6646d05ed03d6e27395077f603dac84d8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-16 23:36:25 -0300

    feat: return options

--- server/routes/loginAccount.py
+++ server/routes/loginAccount.py
@@ -4,4 +4,4 @@ from utils.encrypt import hashText
 def loginAccount(EMAIL_USER, PASSWORD_USER):
   HASH_PASSWORD = hashText(PASSWORD_USER)
 
-  return connectDb.connectUsersTable(f"SELECT * FROM USER_INFORMATION WHERE EMAIL_USER = '{EMAIL_USER}' AND PASSWORD_USER = '{HASH_PASSWORD}'")
+  return connectDb.connectUsersTable(f"SELECT * FROM USER_INFORMATION WHERE EMAIL_USER = '{EMAIL_USER}' AND PASSWORD_USER = '{HASH_PASSWORD}'", True)
