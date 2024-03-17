f3013cb4af9a0481ce1138dda6fd8b2a76ab4338
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-17 14:01:42 -0300

    fix: routes

--- client/src/pages/Home/home.tsx
+++ client/src/pages/Home/home.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import CheckLocalStorage from "@/utils/checkLocalStorageNull";
+
 export default function Home(): JSX.Element {
+  useEffect(() => {
+    CheckLocalStorage();
+  }, []);
+
   return(
     <div> oi</div>
   )
--- client/src/pages/routes.tsx
+++ client/src/pages/routes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "@pages/Home/home";
+import Login from "@pages/Login/login";
 
 export default function Router(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
