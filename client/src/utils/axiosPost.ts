5c59660a8a1241f98aad84d4af4ca7a96adce540
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-17 17:26:51 -0300

    chore: data manipulation

--- client/src/utils/axiosPost.ts
+++ client/src/utils/axiosPost.ts
@@ -1,13 +1,30 @@
 import axios, { AxiosResponse } from "axios";
 
+interface Data {
+  EMAIL_USER: string,
+  PASSWORD_USER: string,
+  CELLPHONE_USER?: string,
+  NAME_USER?: string,
+  SCHOOL_ID_SCHOOL: number
+}
+
 interface Props {
   url: string;
-  data: string;
+  data: string[];
+  school_id: number
 }
 
 export default function AxiosPost(props: Props) {
+  const postData:Data = {
+    EMAIL_USER: props.data[0],
+    PASSWORD_USER: props.data[1],
+    CELLPHONE_USER: props.data[2],
+    NAME_USER: props.data[3],
+    SCHOOL_ID_SCHOOL: props.school_id
+  }
+
   try {
-    axios.post(props.url, props.data)
+    axios.post(props.url, postData)
     .then((response: AxiosResponse) => {
       return response.data;
     })
