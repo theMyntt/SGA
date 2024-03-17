a84c713d1d1daa912f3895ac1c50d07b84a071e0
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-17 18:14:08 -0300

    todo: fix later

--- client/src/utils/axiosPost.ts
+++ client/src/utils/axiosPost.ts
@@ -1,37 +1,19 @@
 import axios, { AxiosResponse } from "axios";
 
 interface Data {
-  EMAIL_USER: string,
-  PASSWORD_USER: string,
-  CELLPHONE_USER?: string,
-  NAME_USER?: string,
-  SCHOOL_ID_SCHOOL: number
+  EMAIL_USER: string;
+  PASSWORD_USER: string;
+  CELLPHONE_USER?: string | "";
+  NAME_USER?: string | "";
+  SCHOOL_ID_SCHOOL: number;
 }
 
 interface Props {
   url: string;
-  data: string[];
-  school_id: number
 }
 
-export default function AxiosPost(props: Props) {
-  const postData:Data = {
-    EMAIL_USER: props.data[0],
-    PASSWORD_USER: props.data[1],
-    CELLPHONE_USER: props.data[2],
-    NAME_USER: props.data[3],
-    SCHOOL_ID_SCHOOL: props.school_id
-  }
-
-  try {
-    axios.post(props.url, postData)
-    .then((response: AxiosResponse) => {
-      return response.data;
-    })
-    .catch((error) => {
-      return error;
-    })
-  } catch (error) {
-    return error;
-  }
+export default async function AxiosPost(data: Data, props: Props): Promise<any> {
+  return await axios.post(props.url, data)
+    .then((response: AxiosResponse) => response.data)
+    .catch((error) => { throw error; });
 }
