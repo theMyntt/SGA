2e0dc932e8d1eff3af0a03782b70df2347b4a9c3
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-19 21:54:58 -0300

    fix: import

--- server/routes/getAccount.py
+++ server/routes/getAccount.py
@@ -1,7 +1,7 @@
-from utils import connectDb
+from utils.connectDb import connectUsersTable
 from utils.encrypt import hashText
 
 def loginAccount(EMAIL_USER, PASSWORD_USER, SCHOOL_ID_SCHOOL):
   HASH_PASSWORD = hashText(PASSWORD_USER)
 
-  return connectDb.connectUsersTable(f"SELECT * FROM USER_INFORMATION WHERE EMAIL_USER = '{EMAIL_USER}' AND PASSWORD_USER = '{HASH_PASSWORD}' AND SCHOOL_ID_SCHOOL = '{SCHOOL_ID_SCHOOL}'", True)
+  return connectUsersTable(f"SELECT * FROM USER_INFORMATION WHERE EMAIL_USER = '{EMAIL_USER}' AND PASSWORD_USER = '{HASH_PASSWORD}' AND SCHOOL_ID_SCHOOL = '{SCHOOL_ID_SCHOOL}'", True)
