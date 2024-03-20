a78e22d5cf41b5cfa05bf008542b2cbe567fe872
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-19 22:31:40 -0300

    feat: login page

--- client/src/pages/Login/login.tsx
+++ client/src/pages/Login/login.tsx
@@ -1,9 +1,14 @@
 import LoginForm from "@/components/local/LoginForm/loginForm";
 
+import "./login.scss"
+
 export default function Login(): JSX.Element {
   return (
-    <div>
-      <LoginForm />
+    <div className="container" id="login">
+      <h2 className="to-center">Fazer login.</h2>
+      <div className="card">
+        <LoginForm />
+      </div>
     </div>
   );
 }
