a6384eba1b144f2bb5ffc17b1117acc0d9fa910a
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-19 22:34:36 -0300

    chore: vite resolve typescript alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src/"),
       "@pages": path.resolve(__dirname, "./src/pages/"),
-      "@utils": path.resolve(__dirname, "./src/utils")
+      "@utils": path.resolve(__dirname, "./src/utils/")
     },
   },
 });
