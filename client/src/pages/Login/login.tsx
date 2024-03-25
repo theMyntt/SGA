1ad38c6c7643b13e96157fa0d398e93295295e6b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-24 21:24:09 -0300

    feat: if u have a account, go to home page

--- client/src/pages/Login/login.tsx
+++ client/src/pages/Login/login.tsx
@@ -1,8 +1,15 @@
 import LoginForm from "@/components/local/LoginForm/loginForm";
 
 import "./login.scss"
+import { useEffect } from "react";
 
 export default function Login(): JSX.Element {
+  useEffect(() => {
+    if (localStorage.getItem("name") !== null) {
+      window.location.href = "/home";
+    } 
+  })
+
   return (
     <div className="container" id="login">
       <h2 className="to-center">Fazer login.</h2>
