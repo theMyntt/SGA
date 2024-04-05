2a79ca1c07933d42252c3be1d423b0ef61ac7882
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-04 21:27:57 -0300

    chore: sha256 usage

--- server/data/schema.sql
+++ server/data/schema.sql
@@ -50,7 +50,8 @@ INSERT INTO USER_INFORMATION(
     NAME_USER,
     SCHOOL_ID_SCHOOL
 ) VALUES
-	("admin@admin.org", "12345678", "11990000000", "Admin", 1);
+	("admin@admin.org", "ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f
+", "11990000000", "Admin", 1);
     
 SELECT * 
 FROM USER_INFORMATION 
