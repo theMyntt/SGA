27f527823227fbe39ce44fa6e7d8b14ed62257bd
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-04 21:53:07 -0300

    chore: capitalize usage

--- client/src/pages/Home/home.tsx
+++ client/src/pages/Home/home.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import CheckLocalStorage from "@/utils/checkLocalStorageNull";
 import Header from "@/components/shared/Header/header";
+import { CapitalizeName, CapitalizeOneLetter } from "@/utils/capitalize";
 
 export default function Home(): JSX.Element {
   useEffect(() => {
@@ -11,8 +12,18 @@ export default function Home(): JSX.Element {
     <div>
       <Header />
       <div className="app-screen">
-        <h2>Bem vindo, {window.localStorage.getItem("name")}</h2>
+        <h2>
+          Bem vindo, {CapitalizeOneLetter(window.localStorage.getItem("name") ?? "")}
+        </h2>
         <p>O que quer fazer hoje?</p>
+
+        <div className="student-info card">
+          <h2>Indentificação do aluno.</h2>
+          <div>
+            <p>Nome:</p>
+            <p>{CapitalizeName(localStorage.getItem("name") ?? "", localStorage.getItem("lastName") ?? "")}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
