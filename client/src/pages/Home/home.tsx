43be024773d5b6009ecfe855dc60fb36a8542a75
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-04 22:15:31 -0300

    feat: get student basic information

--- client/src/pages/Home/home.tsx
+++ client/src/pages/Home/home.tsx
@@ -1,11 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CheckLocalStorage from "@/utils/checkLocalStorageNull";
 import Header from "@/components/shared/Header/header";
 import { CapitalizeName, CapitalizeOneLetter } from "@/utils/capitalize";
+import axios, { AxiosResponse } from "axios";
+import { dev } from "@config/environment";
 
 export default function Home(): JSX.Element {
+  const [schoolInfo, setSchoolInfo] = useState<AxiosResponse>();
+
   useEffect(() => {
     CheckLocalStorage();
+
+    async function getInfo(): Promise<void> {
+      try {
+        const response: AxiosResponse<any> = await axios.get(
+          `http://${dev.baseUrl + dev.port + dev.routes.getSchoolInfoRoute}`,
+          { params: { id: localStorage.getItem("school") } }
+        );
+        setSchoolInfo(response)
+      } catch (error) {
+        console.error('Erro ao obter informações da escola:', error);
+      }
+    }
+  
+    getInfo();
   }, []);
 
   return (
@@ -13,15 +31,27 @@ export default function Home(): JSX.Element {
       <Header />
       <div className="app-screen">
         <h2>
-          Bem vindo, {CapitalizeOneLetter(window.localStorage.getItem("name") ?? "")}
+          Bem vindo,{" "}
+          {CapitalizeOneLetter(window.localStorage.getItem("name") ?? "")}
         </h2>
         <p>O que quer fazer hoje?</p>
-
+        <br />
+        <hr />
+        <br />
         <div className="student-info card">
           <h2>Indentificação do aluno.</h2>
           <div>
+            <p>Id do Aluno:</p>
+            <p className="card">{localStorage.getItem("id") ?? ""}</p>
             <p>Nome:</p>
-            <p>{CapitalizeName(localStorage.getItem("name") ?? "", localStorage.getItem("lastName") ?? "")}</p>
+            <p className="card">
+              {CapitalizeName(
+                localStorage.getItem("name") ?? "",
+                localStorage.getItem("lastName") ?? ""
+              )}
+            </p>
+            <p>Escola:</p>
+            <p className="card">{schoolInfo?.data[0][1]}</p>
           </div>
         </div>
       </div>
