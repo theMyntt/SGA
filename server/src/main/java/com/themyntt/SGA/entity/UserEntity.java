b50e5020c1eb6bdb84e4af694768f0e40fc2ec8f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-19 22:07:45 -0300

    feat: user entity

---
+++ server/src/main/java/com/themyntt/SGA/entity/UserEntity.java
@@ -0,0 +1,43 @@
+package com.themyntt.SGA.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "USER_INFORMATION")
+public class UserEntity {
+    @Id
+    @Column(name = "ID_USER")
+    public String id;
+
+    @Column(name = "EMAIL_USER")
+    public String email;
+
+    @Column(name = "PASSWORD_USER")
+    public String password;
+
+    @Column(name = "CELLPHONE_USER")
+    public String cellphone;
+
+    @Column(name = "CPF_USER")
+    public String cpf;
+
+    @Column(name = "STATE_CPF_USER")
+    public String stateCpf;
+
+    @Column(name = "RG_USER")
+    public String rg;
+
+    @Column(name = "FIRST_NAME_USER")
+    public String firstName;
+
+    @Column(name = "LAST_NAME_USER")
+    public String lastName;
+
+    @Column(name = "SCHOOL_ID_SCHOOL")
+    public String schoolId;
+
+    public UserEntity() { }
+}
