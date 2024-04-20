ca3c2525226538f11fb508cf30c442c656aa3e6a
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-19 21:11:49 -0300

    feat: create int id

--- server/utils/createId.py
+++ server/utils/createId.py
@@ -5,3 +5,6 @@ def randomChoice() -> str:
 
 def createStringId() -> str:
   return f"{randomChoice()}-{randomChoice()}-{randomChoice()}-{randomChoice()}"
+
+def createIntId() -> int:
+  return random.randint(100000000000000000000, 999999999999999999999)
