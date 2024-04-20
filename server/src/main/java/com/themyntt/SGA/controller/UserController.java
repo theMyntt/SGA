041abf7d14ca2f7cb8673f6ee2afd668ff845964
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-19 22:40:39 -0300

    feat: cpf validator

--- server/src/main/java/com/themyntt/SGA/controller/UserController.java
+++ server/src/main/java/com/themyntt/SGA/controller/UserController.java
@@ -6,17 +6,61 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/user/")
 public class UserController {
     private UserRepository userRepository;
 
+    public String createId() {
+        String id = UUID.randomUUID().toString();
+        String formatId = id.replaceAll("-", "").substring(0, 15);
+        return formatId + "-" + formatId + "-" + formatId;
+    }
+
+    public static boolean checkCPF(String cpf) {
+        cpf = cpf.replaceAll("[^0-9]", "");
+
+        if (cpf.length() != 11) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            sum += (cpf.charAt(i) - '0') * (10 - i);
+        }
+        int digit1 = 11 - (sum % 11);
+        if (digit1 > 9) {
+            digit1 = 0;
+        }
+
+        sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += (cpf.charAt(i) - '0') * (11 - i);
+        }
+        int digit2 = 11 - (sum % 11);
+        if (digit2 > 9) {
+            digit2 = 0;
+        }
+
+        boolean result = (cpf.charAt(9) - '0') == digit1 && (cpf.charAt(10) - '0') == digit2;
+        if (result) {
+            return true;
+        }
+        return false;
+    }
+
     @PostMapping("/set/")
     public String setUser(@RequestBody() UserEntity userInfo) {
         try {
+            boolean check = checkCPF(userInfo.cpf);
+            if (!check) {
+                return "CPF Invalido";
+            }
+
             this.userRepository.setUser(
-                    userInfo.id,
+                    createId(), // gerar userInfo.id
                     userInfo.email,
                     userInfo.password,
                     userInfo.cellphone,
